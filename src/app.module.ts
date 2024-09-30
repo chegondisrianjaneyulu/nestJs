e0b7c1072d2608287990c580ad00964969ec4efb
sri <chegondisrianjaneyulu@gmail.com>
2024-09-30 17:48:15 +0530

    established mysql db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
+import { EmployeeService } from './employee/employee.service';
+import { EmployeeController } from './employee/employee.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EmployeeController],
+  providers: [AppService, PrismaService, EmployeeService],
 })
 export class AppModule {}

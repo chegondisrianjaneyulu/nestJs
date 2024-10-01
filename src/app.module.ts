7a6b3807eb62c9d42cdb03a95a370da65837722b
sri <chegondisrianjaneyulu@gmail.com>
2024-10-01 14:20:13 +0530

    added prefix and cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmployeesModule } from './employees/employees.module';
 import { DatabaseModule } from './database/database.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [UsersModule, EmployeesModule, DatabaseModule],
+  imports: [UsersModule, EmployeesModule, DatabaseModule,
+    ThrottlerModule.forRoot([{
+      name: 'short',
+      ttl: 1000,
+      limit:3
+    }, 
+    {
+      name: 'long',
+      ttl: 60000,
+      limit: 100
+    }
+  ])
+  ]
+  ,
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
